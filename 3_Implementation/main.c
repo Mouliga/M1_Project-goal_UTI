d8a5a5cdca3e8947d9c2bef402483e46fd7a96c4
Mouliga <70700323+Mouliga@users.noreply.github.com>
2022-02-11 23:53:50 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,1 +1,33 @@
 
+#include<stdio.h>
+
+int main()
+{
+  int p,balance = 3000, withdraw = 0, deposit,amount=0,c;
+  printf(" INSERT THE CARD");
+  printf(" Enter the PIN number");
+  scanf("%d",&p);
+  
+  printf(" \n 1.Check Balance\n");
+  printf(" \n 2.Withdraw\n");
+  
+  if(c==1)
+  {
+    printf("Your available balance is: %d", balance);
+  }
+  
+  else if(c==2)
+  {
+    printf("Enter the amount to withdraw");
+     if(amount > balance)
+       printf(" You have insufficient amount");
+     else
+       balance = balance - amount;
+       printf(" Available balance is: %d",balance);
+  }
+  
+  else 
+  {
+    printf("Invalid input");
+  }
+}
