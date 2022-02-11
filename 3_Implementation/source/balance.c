9c242eccd0666b916ec588eab304c3ad907d2739
Mouliga <70700323+Mouliga@users.noreply.github.com>
2022-02-11 21:11:11 +0530

    Update balance.c

--- 3_Implementation/source/balance.c
+++ 3_Implementation/source/balance.c
@@ -1,1 +1,11 @@
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int Balance(int bal)
+{
+  printf("See your Balance");
+  printf(" \n Available Balance is:\n %d,bal);
+  return bal;
+}        
