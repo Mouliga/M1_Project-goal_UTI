e433af4292dfeadd60d1044cb2fb53647a46ebee
Mouliga <70700323+Mouliga@users.noreply.github.com>
2022-03-03 20:57:15 +0530

    Update withdraw.c

--- 3_Implementation/source/withdraw.c
+++ 3_Implementation/source/withdraw.c
@@ -2,21 +2,35 @@
 #include<stdlib.h>
 #include<math.h>
 
-int Withdraw(int wd)
-{
-  int Bal;
-  scanf("%d",&Bal);
-  
-  printf(" Withdraw a money\n");
-  
-  if(Wd<Bal)
-  {
-    Bal -= Wd;
-    printf("Withdrawing money is: %d,wd);
-  }
-           
-  else
-  {
-    printf("You Don't have enough money");
-  }
-  }
+float moneyWithdraw(float balance) {
+    float withdraw;
+    bool back = true;
+
+    printf("You choose to Withdraw a money\n");
+    printf("Your Balance is: $%.2f\n\n", balance);
+
+    while (back) {
+    printf("Enter your amount to withdraw:\n");
+    scanf("%f", &withdraw);
+
+
+    if (withdraw < balance) {
+        back = false;
+        balance -= withdraw;
+        printf("\nYour withdrawing money is:  $%.2f\n", withdraw);
+        printf("**Your New Balance is:   $%.2f\n\n", balance);
+
+    }
+
+        else  {
+
+        printf("+++You don't have enough money+++\n");
+        printf("Please contact to your Bank Customer Services\n");
+        printf("**Your Balance is:   $%.2f\n\n", balance);
+
+    }
+    }
+    return balance;
+
+
+}
